81daac800bb25147180630f01515d6130f05bf36
root <root@chris-laptop.(none)>
2014-01-30 23:01:27 -0800

    yep

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -48,6 +48,30 @@ class User_Controller extends CI_Controller {
     }
 }
 
+class Manager_Controller extends CI_Controller {
+    protected $the_user;
+    
+    public function __construct() {
+        
+        parent::__construct();
+        
+        $data = new stdClass();
+        
+        if($this->ion_auth->in_group('manager')) {
+            $this->the_user = $this->ion_auth
+                ->user()
+                ->row();
+
+            $data->the_user = $this->the_user;
+            $this->load->vars($data);
+        }
+        else {
+            redirect('/');
+        }
+        
+    }
+}
+
 class Common_Auth_Controller extends CI_Controller {
 
     protected $the_user;
