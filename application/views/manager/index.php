aeb9be280fd67f13c1b9e5e8bc0a0fe5ce91fb89
root <root@chris-laptop.(none)>
2014-01-30 23:14:05 -0800

    Addition of nav view for common nav in sections

--- application/views/manager/index.php
+++ application/views/manager/index.php
@@ -43,13 +43,7 @@
                         </div>
                     </div>
                 </div>
-                <div data-role="navbar">
-                    <ul>
-                        <li><a href="<?php echo site_url("manager/home"); ?>" class="ui-btn-active">Today</a></li>
-                        <li><a href="<?php echo site_url("manager/home/week"); ?>">This Week</a></li>
-                        <li><a href="<?php echo site_url("manager/home/month"); ?>">This Month</a></li>
-                    </ul>
-                </div>
+                <?php $this->load->view('nav');?>
             </div>
         </div>
     </body>
