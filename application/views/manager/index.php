9ed0aedd6140157d0446a6ce23d358664d63810b
root <root@chris-laptop.(none)>
2014-01-30 23:11:56 -0800

    Changes in links for manager section placeholder nav

--- application/views/manager/index.php
+++ application/views/manager/index.php
@@ -45,9 +45,9 @@
                 </div>
                 <div data-role="navbar">
                     <ul>
-                        <li><a href="<?php echo site_url("admin/home"); ?>" class="ui-btn-active">Today</a></li>
-                        <li><a href="<?php echo site_url("admin/home/week"); ?>">This Week</a></li>
-                        <li><a href="<?php echo site_url("admin/home/month"); ?>">This Month</a></li>
+                        <li><a href="<?php echo site_url("manager/home"); ?>" class="ui-btn-active">Today</a></li>
+                        <li><a href="<?php echo site_url("manager/home/week"); ?>">This Week</a></li>
+                        <li><a href="<?php echo site_url("manager/home/month"); ?>">This Month</a></li>
                     </ul>
                 </div>
             </div>
