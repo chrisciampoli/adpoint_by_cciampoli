3bd347b31f2366e01b92cde859df98e846772a0f
root <root@chris-laptop.(none)>
2014-01-31 22:58:52 -0800

    done

--- application/views/manager/navigation/nav.php
+++ application/views/manager/navigation/nav.php
@@ -1,7 +1,7 @@
 <div data-role="navbar">
     <ul>
-        <li><a href="<?php echo site_url("manager/home"); ?>" class="ui-btn-active">Today</a></li>
-        <li><a href="<?php echo site_url("manager/home/week"); ?>">This Week</a></li>
-        <li><a href="<?php echo site_url("manager/home/month"); ?>">This Month</a></li>
+        <li><a href="<?php echo site_url("manager/home"); ?>">Dashboard</a></li>
+        <li><a href="<?php echo site_url("manager/home/employees"); ?>">Employees</a></li>
+        <li><a href="<?php echo site_url("manager/home/staff"); ?>">Settings</a></li>
     </ul>
 </div>
