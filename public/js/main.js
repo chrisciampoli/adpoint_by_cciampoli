54fa86bbe3aa5c18a6f5997f79bf41e71b6e01cf
root <root@chris-laptop.(none)>
2014-02-02 01:14:35 -0800

    borg

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 $(function(){
    $('body').on('click','input#logout_btn',function(e){
       e.preventDefault(); 
-      $.post('/index.php/auth/logout');
+      $.post('swift_schedules/index.php/auth/logout');
    });
 });
