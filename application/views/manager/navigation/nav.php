66030018482e2036384df8e9bdf8f88f04d7d784
root <root@chris-laptop.(none)>
2014-02-02 00:15:02 -0800

    changes

--- application/views/manager/navigation/nav.php
+++ application/views/manager/navigation/nav.php
@@ -2,6 +2,6 @@
     <ul>
         <li><a href="<?php echo site_url("manager/home"); ?>">Dashboard</a></li>
         <li><a href="<?php echo site_url("manager/home/employees"); ?>">Employees</a></li>
-        <li><a href="<?php echo site_url("manager/home/staff"); ?>">Settings</a></li>
+        <li><a href="<?php echo site_url("manager/home/settings"); ?>">Settings</a></li>
     </ul>
 </div>
