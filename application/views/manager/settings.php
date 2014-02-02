9f76fffc9a38f88271109af1f2c35a3821fc8cf9
root <root@chris-laptop.(none)>
2014-02-02 00:19:59 -0800

    changes

--- application/views/manager/settings.php
+++ application/views/manager/settings.php
@@ -1,43 +1,43 @@
-    <body>
-        <div data-role="page" id="page1">
-            <div data-role="header" style="background-color:#ADFCC0">
-                <h1>Swift Schedules</h1>
-            </div>
-            <div role="main" class="ui-content">
-                <div data-role="tabs" style="border:2px solid black;">
-                    <div id="fragment-1">
-                        <div height="100%" id="container">
-                            <div id="employee_name" width="100%"><h2 class="ui-bar">Employee Name</h2></div>
-                            <div id="current_date" width="100%" style="background-color:#C5FAF4"><h2 class="ui-bar"><?php echo date('l F jS Y'); ?></h2></div>
-                            <div id="assigned_work_area" width="100%" style="background-color:#F3FF89"><h2 class="ui-bar">Section Working</h2></div>
-                            <div id="employee_table">
-                               <table width="100%">
-                                    <thead>
-                                    <th>Type</th>
-                                    <th>Name</th>
-                                    <th>Position</th>
-                                    <th>Phone</th>
-                                    <th>Email</th>
-                                    <th>Availability</th>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td>Manager</td>
-                                            <td>Bob</td>
-                                            <td>Floor</td>
-                                            <td>619-999-3453</td>
-                                            <td>test@test.com</td>
-                                            <td>open</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                              <input type="submit" value="Logout">
-                            </div>
+<body>
+    <div data-role="page" id="page1">
+        <div data-role="header" style="background-color:#ADFCC0">
+            <h1>Swift Schedules</h1>
+        </div>
+        <div role="main" class="ui-content">
+            <div data-role="tabs" style="border:2px solid black;">
+                <div id="fragment-1">
+                    <div height="100%" id="container">
+                        <div id="employee_name" width="100%"><h2 class="ui-bar">Employee Name</h2></div>
+                        <div id="current_date" width="100%" style="background-color:#C5FAF4"><h2 class="ui-bar"><?php echo date('l F jS Y'); ?></h2></div>
+                        <div id="assigned_work_area" width="100%" style="background-color:#F3FF89"><h2 class="ui-bar">Section Working</h2></div>
+                        <div id="employee_table">
+                            <table data-role="table" id="my-table" data-mode="reflow">
+                                <thead>
+                                    <tr>
+                                        <th>Rank</th>
+                                        <th>Movie Title</th>
+                                        <th>Year</th>
+                                        <th><abbr title="Rotten Tomato Rating">Rating</abbr></th>
+                                        <th>Reviews</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <th>1</th>
+                                        <td><a href="foo.com" data-rel="external">Citizen Kane</a></td>
+                                        <td>1941</td>
+                                        <td>100%</td>
+                                        <td>74</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <input type="submit" value="Logout">
                         </div>
                     </div>
                 </div>
-                <?php $this->load->view('manager/navigation/nav');?>
             </div>
+            <?php $this->load->view('manager/navigation/nav'); ?>
         </div>
-    </body>
+    </div>
+</body>
 </html>
