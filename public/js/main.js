41b620abcad3d7ad7ce7a17a04471a3f5d437440
root <root@chris-laptop.(none)>
2014-02-02 09:10:54 -0800

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,5 @@
 $(function(){
-    $('body').on('click','#logout_btn',function(e){
+    $('#container').on('click','input#logout_btn',function(e){
         e.preventDefault();
         $.post('http://54.193.89.75/swift_schedules/index.php/auth/logout').always(function(){
             window.location.replace("http://54.193.89.75/swift_schedules/index.php/auth/login");
