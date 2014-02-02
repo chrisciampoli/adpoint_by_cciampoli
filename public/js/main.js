559b47ec051f8a283c33c2b94f29ba6f363ff566
root <root@chris-laptop.(none)>
2014-02-02 08:58:46 -0800

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,8 @@
 $(function(){
     $('#logout_btn').on('click',function(e){
         e.preventDefault();
-        $.post('http://54.193.89.75/swift_schedules/index.php/auth/logout');
+        $.post('http://54.193.89.75/swift_schedules/index.php/auth/logout').always(function(){
+            console.log('Complete!');
+        });
     });
 });
