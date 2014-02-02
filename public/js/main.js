cbe907ffd31b1482c336f9c8ff31504fab89a5ba
root <root@chris-laptop.(none)>
2014-02-02 01:06:12 -0800

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,6 @@
 $(function(){
    $('body').on('click','input#logout_btn',function(e){
       e.preventDefault();
-      var url = "<?php base_url('auth/logout');?>";
-      $.post(url);
+      $.post('/auth/logout');
    });
 });
