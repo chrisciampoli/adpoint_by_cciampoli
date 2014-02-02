d1ef4118d0c08fea06a2b7d302d4dc6dc5c1e74f
root <root@chris-laptop.(none)>
2014-02-02 09:16:49 -0800

    changes

--- application/controllers/manager/home.php
+++ application/controllers/manager/home.php
@@ -5,27 +5,22 @@ class Home extends Manager_Controller {
     function __construct() {
         parent::__construct();
         $this->load->library('session');
+        $this->load->view('manager/head');
     }
     
     function index() {
         $data['name'] = $this->session->userdata('username');
-        $this->load->view('manager/head');
         $this->load->view('manager/index', $data);
-        $this->load->view('footer');
     }
     
     function employees() {
         $data['name'] = $this->session->userdata('username');
-        $this->load->view('manager/head');
         $this->load->view('manager/employees', $data);
-        $this->load->view('footer');
     }
     
     function settings() {
         $data['name'] = $this->session->userdata('username');
-        $this->load->view('manager/head');
         $this->load->view('manager/settings', $data);
-        $this->load->view('footer');
     }
     
 }
