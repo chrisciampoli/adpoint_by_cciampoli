3921148db9932ee990dca573e3d117b45332272c
root <root@chris-laptop.(none)>
2014-02-02 21:36:00 -0800

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,9 @@
 $(function() {
     $('#table_header').prepend('<input id="add_employee_btn" type="button" value="Add Employee" />');
     $('body').on('click', 'input#add_employee_btn', function(e) {
-        var add_dialog = String(
-                '<div><label for="first_name">First Name</label></div>'+
-                '<div><input type="text" name="first_name" id="first_name" /></div>'
-                );
+        var add_dialog = String()
+                +'<div><label for="first_name">First Name</label></div>'
+                +'<div><input type="text" name="first_name" id="first_name" /></div>';
         $(add_dialog).dialog({
             title: "Add Employee"
         });
