66388e9a306affd918a71df48d239270f1c8d21e
root <root@chris-laptop.(none)>
2014-02-02 18:38:47 -0800

    changes

--- application/views/manager/head.php
+++ application/views/manager/head.php
@@ -10,7 +10,58 @@
         <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css" />
         <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
         <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
-        <script src="<?php echo base_url('public/js/main.js');?>"></script>
+        <script src="<?php echo base_url('public/js/main.js'); ?>"></script>
+        <style>
+            table{
+                border: 0px solid black;
+                border-spacing: 0px;
+            }
+
+            table thead tr{
+                font-family: Arial, monospace;
+                font-size: 14px;
+            }
+
+            table thead tr th{
+                border-bottom: 2px solid black;
+                border-top: 1px solid black;
+                margin: 0px;
+                padding: 2px;
+                background-color: #cccccc;
+            }
+
+            table tr {
+                font-family: arial, monospace;
+                color: black;
+                font-size:12px;
+                background-color: white;
+            }
+
+            table tr.odd {
+                background-color: #AAAAAA;
+            }
+
+            table tr td, th{
+                border-bottom: 1px solid black;
+                padding: 2px;
+            }
+
+            a:link{
+                font-family:arial, monospace;
+                text-decoration: none;
+                color: teal;
+            }
+
+            a:hover{
+                text-decoration: underline;
+            }
+
+            a:visited{
+                color:black;
+                text-decoration: none;
+            }
+
+        </style>
         <title>Swift Schedules</title>
-        
+
     </head>
