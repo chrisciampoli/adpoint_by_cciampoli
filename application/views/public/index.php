bd4b241e28a6e526744f56ec6568dccfa86c2093
root <root@chris-laptop.(none)>
2014-02-02 17:30:08 -0800

    changes

--- application/views/public/index.php
+++ application/views/public/index.php
@@ -1,35 +1,5 @@
-
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
-
-    <title>Swift Schedules</title>
-
-    <!-- Bootstrap core CSS -->
-    <!--<link href="<?php //echo base_url('public/css/bootstrap.css')?>" rel="stylesheet">-->
-    <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.0/jquery.mobile-1.4.0.min.css" />
-    <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-    <script src="http://code.jquery.com/mobile/1.4.0/jquery.mobile-1.4.0.min.js"></script>
-    <!-- Custom styles for this template -->
-    <!--<link href="jumbotron.css" rel="stylesheet">-->
-
-    <!-- Just for debugging purposes. Don't actually copy this line! -->
-    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
-  </head>
-
-  <body>
+<?php $this->load->view('manager/head');?>
+<body>
 
     <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
       <div class="container" style="background-color:#ADFCC0">
