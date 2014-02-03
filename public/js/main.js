7ae89407e6cdc2d825554e7339412008038551f4
root <root@chris-laptop.(none)>
2014-02-02 20:59:01 -0800

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,13 @@
-$(function(){
-    
-    $('#container').on('click','input#logout_btn',function(e){
+$(function() {
+    $('#table_header').prepend('<input id="add_employee_btn" type="button" value="Add Employee" />');
+    $('body').on('click', 'input#add_employee_btn', function(e) {
+        $('<div>Test</div>').dialog({
+            title: "Add Employee"
+        });
+    });
+    $('#container').on('click', 'input#logout_btn', function(e) {
         e.preventDefault();
-        $.post('http://54.193.89.75/swift_schedules/index.php/auth/logout').always(function(){
+        $.post('http://54.193.89.75/swift_schedules/index.php/auth/logout').always(function() {
             window.location.replace("http://54.193.89.75/swift_schedules/index.php/auth/login");
         });
     });
