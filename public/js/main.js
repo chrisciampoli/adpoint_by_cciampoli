6960b77e5daf5c604efbf4a80ca7ab8b78d4812a
root <root@chris-laptop.(none)>
2014-02-02 21:46:43 -0800

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -5,7 +5,7 @@ $(function() {
                 +'<div id="add_dialog"><div><label for="first_name">First Name</label></div>'
                 +'<div><input type="text" name="first_name" id="first_name" /></div>';
                 +'<div><label for="last_name">Last Name</label></div>'
-                +'<div><input type="text" name="last_name" id="last_name" /></div>';
+                +'<div><input type="text" name="last_name" id="last_name" /></div></div>';
         $(add_dialog).dialog({
             title: "Add Employee"
         });
