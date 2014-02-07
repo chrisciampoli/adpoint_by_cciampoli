c63915f574a4e6aa10afd7181a02e9dcf1090eda
root <root@chris-laptop.(none)>
2014-02-06 21:50:56 -0800

    changes

--- public/js/employees.js
+++ public/js/employees.js
@@ -1,3 +1,85 @@
-$(function(){
-   alert('Loaded baby!'); 
+$(function(){ 
+   var add_employee = String()
+            + '<section>'
+                + '<div>'
+                        + '<div><label for="first_name">First Name</label><input type="text" name="first_name" id="first_name" /></div>'
+                        + '<div><label for="last_name">Last Name</label><input type="text" name="last_name" id="last_name" /></div>'
+                        + '<div><label for="position">Position</label><input type="text" name="position" id="position" /></div>'
+                        + '<div><label for="phone">Phone</label><input type="text" name="phone" id="phone" /></div>'
+                        + '<div><label for="email">Last Name</label><input type="text" name="email" id="email" /></div>'
+                        + '<div><label for="availability">Availability</label><input type="text" name="availability" id="availability" /></div>'
+                + '</div>'
+            + '</section>';
+    
+    
+    $('#table_header').prepend('<input id="add_employee_btn" type="button" value="Add Employee" />');
+    $('body').on('click', 'input#add_employee_btn', function(e) {
+        $(add_employee).dialog({
+            title: "Add Employee"
+        });
+    });
 });
+
+/*
+ * Pull list of employees from db and returns json
+ */
+function getEmployees() {
+    ajaxData();
+}
+
+function getEmployeesSuccess() {
+    
+}
+
+function getEmployeesFailure() {
+    
+}
+
+function saveEmployee() {
+    
+}
+
+function saveEmployeeSuccess() {
+    
+}
+
+function saveEmployeeFailure() {
+    
+}
+
+function removeEmployee() {
+    
+}
+
+function removeEmployeeSuccess() {
+    
+}
+
+
+function removeEmployeeFailure() {
+    
+}
+
+function updateEmployee() {
+    
+}
+
+function updateEmployeeSuccess() {
+    
+}
+
+function updateEmployeeFailure() {
+    
+}
+
+function renderRows() {
+    
+}
+
+function renderDialog() {
+    
+}
+
+function ajaxData() {
+    $.ajax({});
+}
