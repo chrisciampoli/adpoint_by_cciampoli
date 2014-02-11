32b311323c839f16e0da7968e069fc94f9280d01
root <root@chris-laptop.(none)>
2014-02-10 19:57:33 -0800

    changes

--- application/views/manager/index.php
+++ application/views/manager/index.php
@@ -1,33 +1,96 @@
-    <body>
-        <div data-role="page" id="page1">
-            <div data-role="header" style="background-color:#ADFCC0">
-                <h1>Swift Schedules</h1>
-            </div>
-            <div role="main" class="ui-content">
-                <div data-role="tabs" style="border:2px solid black;">
-                    <div id="fragment-1">
-                        <div height="100%" id="container">
-                                <?php $this->load->view('employee_info'); ?>
-                                <div id="shift_sub_info1" width="100%" class="ui-bar">Shift Time</div>
-                                <div class="ui-btn ui-input-btn ui-shadow">
-                                    Swift Give-up
-                                    <input type="button" data-corners="false" data-enhanced="true" value="The Button"></input>
-                                </div>
-                                <div class="ui-btn ui-input-btn ui-shadow">
-                                    Busy or Not
-                                    <input type="button" data-corners="false" data-enhanced="true" value="The Button"></input>
-                                </div>
-                            </div>
-                            <div class="ui-btn ui-input-btn ui-shadow">
-                                <?php echo form_open("public/auth/logout");?>
-                                <?php echo form_submit('logout_submit', 'Logout');?>
-                                <?php echo form_close();?>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <?php $this->load->view('manager/navigation/nav');?>
-            </div>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
+
+    <title>Dashboard Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
+
+
+    <!-- Custom styles for this template -->
+    <link href="<?php echo base_url('public/css/dashboard.css')?>" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
         </div>
-    </body>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Dashboard</a></li>
+            <li><a href="#">Settings</a></li>
+            <li><a href="#">Profile</a></li>
+            <li><a href="#">Help</a></li>
+          </ul>
+          <form class="navbar-form navbar-right">
+            <input type="text" class="form-control" placeholder="Search...">
+          </form>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <h1 class="page-header">Dashboard</h1>
+
+          <div class="row placeholders">
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img data-src="holder.js/200x200/auto/sky" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Swift Pickup</h4>
+              <span class="text-muted">Manage Swift Pickups</span>
+            </div>
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Busy or Not</h4>
+              <span class="text-muted">Approve Busy or Not Requests</span>
+            </div>
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img data-src="holder.js/200x200/auto/sky" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Employees</h4>
+              <span class="text-muted">Manage Employees</span>
+            </div>
+            <div class="col-xs-6 col-sm-3 placeholder">
+              <img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail">
+              <h4>Settings</h4>
+              <span class="text-muted">Account Settings</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
+    <script src="<?php echo base_url('public/js/docs.js');?>"></script>
+  </body>
 </html>
