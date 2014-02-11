8189f4fb9065756186fa3e69bb9e3b244bccffe3
root <root@chris-laptop.(none)>
2014-02-10 22:40:04 -0800

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -2,16 +2,9 @@ $(function() {
     $(".check_avail").on('click', function(e) {
         e.preventDefault();
         var rel = $(this).attr('rel');
-        $('<div>Availability</div>').dialog({
-            open: function() {
-                $("#datepicker_" + rel).datepicker({
-                    onSelect: function() {
-                        $('<div>Location</div>').dialog();
-                    }
-                });
-            },
-            close: function() {
-                $('#datepicker_' + rel).datepicker('destroy');
+        $("#datepicker_" + rel).datepicker({
+            onSelect: function() {
+                $('<div>Location</div>').dialog();
             }
         });
     });
