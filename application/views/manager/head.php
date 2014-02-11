6cf05ac940b2b434989264eb9aa187ca261a1e66
root <root@chris-laptop.(none)>
2014-02-10 18:14:41 -0800

    changes

--- application/views/manager/head.php
+++ application/views/manager/head.php
@@ -7,10 +7,13 @@
         <meta name="description" content="">
         <meta name="author" content="">
         <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
-        <link rel="stylesheet" href="http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css" />
+        <!-- Bootstrap core CSS -->
+        <link href="<?php echo base_url('public/css/bootstrap.min.css');?>" rel="stylesheet">
+        <!--<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css" />-->
         <link rel="stylesheet" href="<?php echo base_url('public/css/main.css');?>" />
         <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
-        <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
+        <script src="<?php echo base_url('public/js/bootstrap.min.js');?>"></script>
+        <!--<script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>-->
         <script src="<?php echo base_url('public/js/main.js'); ?>"></script>
         <title>Swift Schedules</title>
     </head>
