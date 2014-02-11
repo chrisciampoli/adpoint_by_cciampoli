43d08af3cd23815c66b18ed59f77cd70e488dd0e
root <root@chris-laptop.(none)>
2014-02-10 20:05:29 -0800

    changes

--- application/views/manager/index.php
+++ application/views/manager/index.php
@@ -62,22 +62,22 @@
 
           <div class="row placeholders">
             <div class="col-xs-6 col-sm-3 placeholder">
-              <a href='<?php echo site_url('manager/home/swift_pickup');?>'><img data-src="holder.js/200x200/auto/sky" class="img-responsive" alt="Generic placeholder thumbnail">
+                <a href='<?php echo site_url('manager/home/swift_pickup');?>'><img data-src="holder.js/200x200/auto/sky" class="img-responsive" alt="Generic placeholder thumbnail"></a>
               <h4>Swift Pickup</h4>
               <span class="text-muted">Manage Swift Pickups</span>
             </div>
             <div class="col-xs-6 col-sm-3 placeholder">
-              <img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail">
+                <a href='<?php echo site_url('manager/home/busy_or_not');?>'><img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail"></a>
               <h4>Busy or Not</h4>
               <span class="text-muted">Approve Busy or Not Requests</span>
             </div>
             <div class="col-xs-6 col-sm-3 placeholder">
-              <img data-src="holder.js/200x200/auto/sky" class="img-responsive" alt="Generic placeholder thumbnail">
+                <a href='<?php echo site_url('manager/home/employees');?>'><img data-src="holder.js/200x200/auto/sky" class="img-responsive" alt="Generic placeholder thumbnail"></a>
               <h4>Employees</h4>
               <span class="text-muted">Manage Employees</span>
             </div>
             <div class="col-xs-6 col-sm-3 placeholder">
-              <img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail">
+                <a href='<?php echo site_url('manager/home/settings');?>'><img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail"></a>
               <h4>Settings</h4>
               <span class="text-muted">Account Settings</span>
             </div>
