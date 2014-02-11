bfa09e376a6a27dc41359eb17e5828306625a464
root <root@chris-laptop.(none)>
2014-02-10 22:46:27 -0800

    changes

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,7 @@ $(function() {
         e.preventDefault();
         var rel = $(this).attr('rel');
         $("#datepicker_" + rel).datepicker({
+            showButtonPanel: true,
             onSelect: function() {
                 $('<div>Location</div>').dialog();
             }
