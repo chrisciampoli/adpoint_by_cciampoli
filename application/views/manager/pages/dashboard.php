d06a949431c981fb293c2ca951391e4ee8dcbb58
root <root@chris-laptop.(none)>
2014-02-11 22:21:05 -0800

    changes

--- application/views/manager/pages/dashboard.php
+++ application/views/manager/pages/dashboard.php
@@ -20,9 +20,14 @@
               <span class="text-muted">Manage Employee Busy or Not requests</span>
             </div>
             <div class="col-xs-6 col-sm-3 placeholder">
+                <a href='<?php echo site_url('manager/home/employees');?>'><img data-src="holder.js/200x200/auto/sky" class="img-responsive" alt="Generic placeholder thumbnail"></a>
+              <h4>Employees</h4>
+              <span class="text-muted">Manage Employees</span>
+            </div>
+            <div class="col-xs-6 col-sm-3 placeholder">
                 <a href='<?php echo site_url('manager/home/settings');?>'><img data-src="holder.js/200x200/auto/vine" class="img-responsive" alt="Generic placeholder thumbnail"></a>
               <h4>Current Labor</h4>
-              <span class="text-muted">Current Labor</span>
+              <span class="text-muted">Labor Forcasting and Reporting</span>
             </div>
           </div>
         </div>
