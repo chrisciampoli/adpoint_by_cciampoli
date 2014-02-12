03f75dff37df457174c603b33c8efbc778f098bb
root <root@chris-laptop.(none)>
2014-02-11 20:48:37 -0800

    changes

--- application/views/manager/wrapper.php
+++ application/views/manager/wrapper.php
@@ -1,6 +1,6 @@
-<?php $this->load->view('manager/head'); ?>
+<?php $this->load->view('manager/head', $styles); ?>
 <body>
-    <?php $this->load->view('manager/nav');?>
+    <?php $this->load->view('manager/navigation/nav');?>
     <?php echo $content;?>
-    <?php $this->load->view('manager/scripts'); ?>
+    <?php $this->load->view('manager/scripts', $scripts); ?>
 </body>
