beb21f82669084f40b0c149d1f03a2a6e6ba460f
root <root@chris-laptop.(none)>
2014-02-11 21:48:02 -0800

    changes

--- application/controllers/manager/home.php
+++ application/controllers/manager/home.php
@@ -1,29 +1,25 @@
 <?php
 
 class Home extends Manager_Controller {
-    
-    public $title = '';
+  
     
     function __construct() {
         parent::__construct();
         $this->load->library('session');
         $this->load->database();
-        $data['styles'][] = 'http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css';
-        $data['styles'][] = 'http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css';
-        
-        $data['title'] = $this->title;
-        $this->load->view('manager/head', $data);
     }
     
     function index() {
         $data['styles'][] =  base_url('public/css/dashboard.css');
+        $data['title'] = $this->title;
+        $data['head'] = $this->load->view('manager/head', $data, true);
         $data['name'] = $this->session->userdata('username');
         $this->load->view('manager/index', $data);
     }
     
     function employees() {
         
-        $this->title = 'Employees';
+        $data['title'] = 'Employees';
         
         $data['scripts'][] = 'https://code.jquery.com/jquery-1.10.2.min.js';
         $data['scripts'][] = 'http://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js';
@@ -32,8 +28,11 @@ class Home extends Manager_Controller {
         $data['scripts'][] = base_url('public/js/docs.js');
         
         $data['styles'][] =  base_url('public/css/dashboard.css');
+        $data['styles'][] = 'http://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css';
+        $data['styles'][] = 'http://code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css';
         
-        
+        $data['title'] = $this->title;
+        $data['head'] = $this->load->view('manager/head', $data, true);
         $data['nav'] = $this->load->view('manager/navigation/nav','',true);
         $data['content'] = $this->load->view('manager/employees2',$data,true);
         $data['script_loader'] = $this->load->view('manager/scripts',$data,true);
