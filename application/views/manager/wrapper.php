245d450b1ec1a7d1a532b259c9e19268e35c7c19
root <root@chris-laptop.(none)>
2014-02-11 20:53:33 -0800

    changes

--- application/views/manager/wrapper.php
+++ application/views/manager/wrapper.php
@@ -1,11 +1,10 @@
-<?php
-
-echo "<pre>" . print_r($styles, true) . "</pre>";
-die();
+<?php 
+$data['styles'] = $styles;
+$data['scripts'] = $scripts;
 ?>
-<?php $this->load->view('manager/head', $styles); ?>
+<?php $this->load->view('manager/head', $data); ?>
 <body>
     <?php $this->load->view('manager/navigation/nav');?>
     <?php echo $content;?>
-    <?php $this->load->view('manager/scripts', $scripts); ?>
+    <?php $this->load->view('manager/scripts', $data); ?>
 </body>
