19b4caef8e800f996d801104eafb05b1710d7f02
root <root@chris-laptop.(none)>
2014-02-11 21:36:03 -0800

    changes

--- application/controllers/manager/home.php
+++ application/controllers/manager/home.php
@@ -32,7 +32,7 @@ class Home extends Manager_Controller {
         $data['scripts'][] = base_url('public/js/docs.js');
         
         $data['nav'] = 'manager/navigation/nav';
-        //$data['content'][] = 'manager/employees2';
+        $data['content'][] = 'manager/employees2';
         $data['script_loader'] = $this->load->view('manager/scripts',$data,true);
         
         $data['name'] = $this->session->userdata('username');
--- application/views/manager/wrapper.php
+++ application/views/manager/wrapper.php
@@ -1,6 +1,6 @@
 <body>
     <?php $this->load->view($nav);?>
-    <?php $this->load->view('manager/employees2');?>
+    <?php $this->load->view($content);?>
     <?=$script_loader;?>
 </body>
 </html>
