a80e5081d8b981fc8ef5b0fa8b4bae5372fca7dc
root <root@chris-laptop.(none)>
2014-03-04 19:36:29 -0800

    changes

--- application/views/admin/index2.php
+++ application/views/admin/index2.php
@@ -13,12 +13,43 @@
     </head>
     <body>
         <div data-role="page" id="city" class="demo-page" data-dom-cache="true" data-theme="b" data-prev="<?=site_url('admin/home');?>" data-next="<?=site_url('admin/home/week');?>" data-url="city">
+            <div data-role="header" style="background-color:#BFBA73;color: black;">
+                <h1>Swift Schedules</h1>
+            </div>
             <div role="main" class="ui-content">
-                <div id="trivia-city" class="trivia ui-content" data-role="popup" data-position-to="window" data-tolerance="50,30,30,30" data-theme="a">
-                    <a href="#" data-rel="back" class="ui-btn ui-btn-right ui-btn-b ui-btn-icon-notext ui-icon-delete ui-corner-all">Close</a>
-                    <p>Here some text.</p>
-                </div><!-- /popup -->
-            </div><!-- /content -->
+                <div data-role="tabs" style="border:2px solid #590E0E;">
+                    <div id="fragment-1">
+                        <div height="100%" id="container">
+                            <div id="employee_name" width="100%" class="info_box"><h2 class="ui-bar"><?=  ucwords($name);?> - Cashier</h2></div>
+                            <div id="current_date" width="100%" class="info_box"><h2 class="ui-bar"><?php echo date('l F jS Y'); ?></h2></div>
+                            <div id="assigned_work_area" width="100%" class="info_box"><h2 class="ui-bar">Section Working - Counter</h2></div>
+                            <div id="shift_info" width="100%" style="border:2px solid black;color:white;">
+                                <div id="shift_sub_info1" width="100%" class="ui-bar"><h1>Shift Time 4:30 - 9:30 PM</h1></div>
+                                <div class="ui-btn ui-input-btn ui-shadow">
+                                    Swift Pick up
+                                    <input type="button" data-corners="false" data-enhanced="true" value="The Button"></input>
+                                </div>
+                                <div class="ui-btn ui-input-btn ui-shadow">
+                                    Busy or Not
+                                    <input type="button" data-corners="false" data-enhanced="true" value="The Button"></input>
+                                </div>
+                            </div>
+                            <div class="ui-btn ui-input-btn ui-shadow">
+                                <?php echo form_open("public/auth/logout");?>
+                                <?php echo form_submit('logout_submit', 'Logout');?>
+                                <?php echo form_close();?>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div data-role="navbar">
+                    <ul>
+                        <li><a href="<?php echo site_url("admin/home"); ?>" class="ui-btn-active">Today</a></li>
+                        <li><a href="<?php echo site_url("admin/home/week"); ?>">This Week</a></li>
+                        <li><a href="<?php echo site_url("admin/home/month"); ?>">This Month</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     </body>
     <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
