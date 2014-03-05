5ce71144bdd2962172daa5cb889857660cefd7da
root <root@chris-laptop.(none)>
2014-03-04 17:25:34 -0800

    changes

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -21,9 +21,9 @@
                 <div data-role="tabs" style="border:2px solid black;">
                     <div id="fragment-1">
                         <div height="100%" id="container">
-                            <div id="employee_name" width="100%"><h2 class="ui-bar"><?=  ucwords($name);?></h2></div>
-                            <div id="current_date" width="100%" style="background-color:#C5FAF4"><h2 class="ui-bar"><?php echo date('l F jS Y'); ?></h2></div>
-                            <div id="assigned_work_area" width="100%" style="background-color:#F3FF89"><h2 class="ui-bar">Section Working</h2></div>
+                            <div id="employee_name" width="100%" style="background-color: #DEB887;"><h2 class="ui-bar"><?=  ucwords($name);?></h2></div>
+                            <div id="current_date" width="100%" style="background-color:#DEB887"><h2 class="ui-bar"><?php echo date('l F jS Y'); ?></h2></div>
+                            <div id="assigned_work_area" width="100%" style="background-color:#DEB887"><h2 class="ui-bar">Section Working</h2></div>
                             <div id="shift_info" width="100%" style="border:2px solid black;">
                                 <div id="shift_sub_info1" width="100%" class="ui-bar">Shift Time 4:30 - 9:30 PM</div>
                                 <div class="ui-btn ui-input-btn ui-shadow">
