3acdda8706c5432393d15a9965ff7035242b6b4c
root <root@chris-laptop.(none)>
2014-03-04 20:22:25 -0800

    changes

--- application/views/admin/index2.php
+++ application/views/admin/index2.php
@@ -14,7 +14,7 @@
     <body>
         <div data-role="page" id="city" class="demo-page" data-dom-cache="true" data-theme="b" data-prev="<?=site_url('admin/home');?>" data-next="<?=site_url('admin/home/week');?>" data-url="city">
             <div role="main" class="ui-content">
-                <div id="trivia-city" class="trivia ui-content" data-role="popup" data-position-to="window" data-tolerance="50,30,30,30" data-theme="a">
+                <div id="trivia-city" class="trivia ui-content" data-role="page" data-position-to="window" data-tolerance="50,30,30,30" data-theme="a">
                     <div data-role="tabs" style="border:2px solid #590E0E;">
                         <div id="fragment-1">
                         <div height="100%" id="container">
