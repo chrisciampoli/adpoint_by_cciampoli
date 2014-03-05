757a012a4081d0f785803eb141ef719c6e69fb49
root <root@chris-laptop.(none)>
2014-03-04 20:24:31 -0800

    changes

--- application/views/admin/index2.php
+++ application/views/admin/index2.php
@@ -15,24 +15,10 @@
         <div data-role="page" id="city" class="demo-page" data-dom-cache="true" data-theme="b" data-prev="<?=site_url('admin/home');?>" data-next="<?=site_url('admin/home/week');?>" data-url="city">
             <div role="main" class="ui-content">
                 <div id="trivia-city" class="trivia ui-content" data-role="page" data-position-to="window" data-tolerance="50,30,30,30" data-theme="a">
-                    <div data-role="tabs" style="border:2px solid #590E0E;">
-                        <div id="fragment-1">
-                        <div height="100%" id="container">
-                            <p>TEST</p>
-                        </div>
-                    </div>
+                    <li><h1>Testing</h1></li>
                 </div>
-                <div data-role="navbar">
-                    <ul>
-                        <li><a href="<?php echo site_url("admin/home"); ?>" class="ui-btn-active">Today</a></li>
-                        <li><a href="<?php echo site_url("admin/home/week"); ?>">This Week</a></li>
-                        <li><a href="<?php echo site_url("admin/home/month"); ?>">This Month</a></li>
-                    </ul>
-                </div>
-                    </div>
-                </div><!-- /popup -->
-            </div><!-- /content -->
-        </div>
+            </div><!-- /popup -->
+        </div><!-- /content -->
     </body>
     <script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
     <script src="http://code.jquery.com/mobile/1.4.0/jquery.mobile-1.4.0.min.js"></script>
