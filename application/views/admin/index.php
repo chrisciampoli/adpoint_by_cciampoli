bb2e3d99ac97c4f68a17afa3efdde04c921c4a87
root <root@chris-laptop.(none)>
2014-03-04 18:29:29 -0800

    changes

--- application/views/admin/index.php
+++ application/views/admin/index.php
@@ -27,7 +27,7 @@
                             <div id="shift_info" width="100%" style="border:2px solid black;color:white;">
                                 <div id="shift_sub_info1" width="100%" class="ui-bar">Shift Time 4:30 - 9:30 PM</div>
                                 <div class="ui-btn ui-input-btn ui-shadow">
-                                    Swift Give-up
+                                    Swift Pick up
                                     <input type="button" data-corners="false" data-enhanced="true" value="The Button"></input>
                                 </div>
                                 <div class="ui-btn ui-input-btn ui-shadow">
