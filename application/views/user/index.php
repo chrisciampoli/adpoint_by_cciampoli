f2c5f085ed485e2f0b95c09b76c842aefbd0ce91
root <root@chris-laptop.(none)>
2014-03-09 23:07:02 -0700

    changes

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -1,90 +1,60 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="shortcut icon" href="../../docs-assets/ico/favicon.png">
 
-    <title>Swift Schedules</title>
+        <title>Swift Schedules</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="<?php echo base_url('public/css/bootstrap.css');?>" rel="stylesheet">
+        <!-- Bootstrap core CSS -->
+        <link href="<?php echo base_url('public/css/bootstrap.css'); ?>" rel="stylesheet">
 
-    <!-- Custom styles for this template -->
-    <link href="sticky-footer-navbar.css" rel="stylesheet">
+        <!-- Custom styles for this template -->
+        <link href="sticky-footer-navbar.css" rel="stylesheet">
 
-    <!-- Just for debugging purposes. Don't actually copy this line! -->
-    <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+        <!-- Just for debugging purposes. Don't actually copy this line! -->
+        <!--[if lt IE 9]><script src="../../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
 
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
-  </head>
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+          <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+        <![endif]-->
+    </head>
 
-  <body>
+    <body>
 
-    <!-- Wrap all page content here -->
-    <div id="wrap">
-
-      <!-- Fixed navbar -->
-      <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-        <div class="container">
-          <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#">Swift Schedules</a>
-          </div>
-          <div class="collapse navbar-collapse">
-            <ul class="nav navbar-nav">
-              <li><a href="<?php echo site_url('user/home/about');?>">About</a></li>
-              <li><a href="<?php echo site_url('user/home/contact');?>">Contact</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Campaign Management<b class="caret"></b></a>
-                <ul class="dropdown-menu">
-                  <li><a href="<?php echo site_url('locations/index');?>">Locations</a></li>
-                  <li><a href="<?php echo site_url('media/index');?>">Media</a></li>
-                  <li><a href="<?php echo site_url('advertising/get_ads');?>">Ads</a></li>
-                  <li class="divider"></li>
-                  <li class="dropdown-header">Schedules</li>
-                  <li><a href="<?php echo site_url('schedules/available');?>">Available</a></li>
-                  <li><a href="<?php echo site_url('schedules/edit_current');?>">Edit Current Schedule</a></li>
-                </ul>
-              </li>
+        <!-- Wrap all page content here -->
+        <div id="wrap">
+            <ul class="nav nav-tabs">
+                <li id="home_tab" class="active"><a href="#">Home</a></li>
+                <li><a href="#">Profile</a></li>
+                <li><a href="#">Messages</a></li>
             </ul>
-          </div><!--/.nav-collapse -->
         </div>
-      </div>
-
-      <!-- Begin page content -->
-      <div class="container">
-        <div class="page-header">
-          <h1>Sticky footer with fixed navbar</h1>
+        <div>
+            
         </div>
-        <p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added within <code>#wrap</code> with <code>padding-top: 60px;</code> on the <code>.container</code>.</p>
-        <p>Back to <a href="../sticky-footer">the default sticky footer</a> minus the navbar.</p>
-      </div>
-    </div>
-
-    <div id="footer">
-      <div class="container">
-        <p class="text-muted credit">Example courtesy <a href="http://martinbean.co.uk">Martin Bean</a> and <a href="http://ryanfait.com/sticky-footer/">Ryan Fait</a>.</p>
-      </div>
-    </div>
-
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
-    <script src="<?php echo base_url('public/js/bootstrap.min.js');?>"></script>
-  </body>
+        <!-- Bootstrap core JavaScript
+        ================================================== -->
+        <!-- Placed at the end of the document so the pages load faster -->
+        <script src="https://code.jquery.com/jquery-1.10.2.min.js"></script>
+        <script src="<?php echo base_url('public/js/bootstrap.min.js'); ?>"></script>
+    </body>
+    <script>
+        $(function() {
+            
+            $('#home_tab').tab('show');
+            
+            $('#home_tab a').click(function(e) {
+                e.preventDefault()
+                $(this).tab('show')
+            })
+            
+        });
+    </script>
 </html>
