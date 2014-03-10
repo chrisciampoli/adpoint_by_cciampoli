a225890a825b3eae4e908eaa47a338c41e552861
root <root@chris-laptop.(none)>
2014-03-10 00:25:58 -0700

    changes

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -41,7 +41,7 @@
         <li data-target="#carousel-example-captions" data-slide-to="2"></li>
       </ol>
       <div class="carousel-inner">
-        <div class="item active" style="background-color:black;height:500px;width:900px;">
+        <div class="item active" style="background-color:black;height:100%;width:100%;">
           <div class="carousel-caption">
             <h3>First slide label</h3>
             <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
