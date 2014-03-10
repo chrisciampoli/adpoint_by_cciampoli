1fc7c90469f4a1c2033cea3bd7393f187f0c992f
root <root@chris-laptop.(none)>
2014-03-10 00:32:06 -0700

    changes

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -26,50 +26,74 @@
         <![endif]-->
         <script>
             var config = {
-                base: "<?php echo base_url(); ?>",
-                notes: 4
+              base: "<?php echo base_url(); ?>",
+              notes: 4
             };
         </script>
     </head>
 
     <body>
-        <div class="bs-example">
-    <div id="carousel-example-captions" class="carousel slide" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#carousel-example-captions" data-slide-to="0" class="active"></li>
-        <li data-target="#carousel-example-captions" data-slide-to="1"></li>
-        <li data-target="#carousel-example-captions" data-slide-to="2"></li>
-      </ol>
-      <div class="carousel-inner">
-        <div class="item active" style="background-color:black;width:100%;height:500px;">
-          <div class="carousel-caption">
-              <div class="row col-xs-12"><h2>Employee Name</h2></div>
-            <h3>Daily</h3>
-          </div>
+
+        <!-- Wrap all page content here -->
+        <div id="container">
+            <ul class="nav nav-tabs">
+                <li id="daily_tab" class="active"><a href="#daily" data-toggle="tab">Daily</a></li>
+                <li><a href="#week" data-toggle="tab">Week</a></li>
+                <li><a href="#month" data-toggle="tab">Month</a></li>
+            </ul>
+            <div class="tab-content">
+                <div class="tab-pane active" id="daily">
+                    <table class="table table-striped table-hover">
+                        <tr>
+                            <td class="well"><h2>Employee Name</h2></td>
+                        </tr>
+                        <tr>
+                            <td class="well"><h3>Date</h3></td>
+                        </tr>
+                        <tr>
+                            <td class="well"><h3>Title - Area</h3></td>
+                        </tr>
+                        <tr>
+                            <td><h3>Shift Hours</h3></td>
+                        </tr>
+                    </table>
+                    
+                    <div class="row">
+                        <button type="button" class="btn btn-primary col-xs-6">Swift Giveup</button>
+                        <button type="button" class="btn btn-primary col-xs-6">Busy or Not</button>
+                    </div>
+                    <div class="row"></div>
+                    <div class="row">
+                        <button type="button" class="btn btn-primary col-xs-12" id="logout_btn">Logout</button>
+                    </div>
+                </div>
+                <div class="tab-pane" id="week">
+                    <div class="row">
+                        <div class="col-xs-12">.col-md-6</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12">.col-md-6</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-xs-12">.col-md-6</div>
+                    </div>
+                </div>
+                <div class="tab-pane" id="month">
+                    <div class="row">
+                        <div class="col-md-12">.col-md-6</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">.col-md-6</div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">.col-md-6</div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div class="item">
-          <img data-src="holder.js/900x500/auto/#666:#666" alt="Second slide image">
-          <div class="carousel-caption">
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </div>
+        <div>
+            
         </div>
-        <div class="item">
-          <img data-src="holder.js/900x500/auto/#555:#5555" alt="Third slide image">
-          <div class="carousel-caption">
-            <h3>Third slide label</h3>
-            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-          </div>
-        </div>
-      </div>
-      <a class="left carousel-control" href="#carousel-example-captions" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left"></span>
-      </a>
-      <a class="right carousel-control" href="#carousel-example-captions" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right"></span>
-      </a>
-    </div>
-  </div><!-- /example -->
         <!-- Bootstrap core JavaScript
         ================================================== -->
         <!-- Placed at the end of the document so the pages load faster -->
