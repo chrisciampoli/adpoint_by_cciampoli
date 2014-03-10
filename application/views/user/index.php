471375a38a412fab8925ba243a3985204c7457a2
root <root@chris-laptop.(none)>
2014-03-09 23:55:44 -0700

    changes

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -24,6 +24,12 @@
           <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
           <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
         <![endif]-->
+        <script>
+            var config = {
+              base: <?php echo base_url(); ?>",
+              notes: 4
+            };
+        </script>
     </head>
 
     <body>
