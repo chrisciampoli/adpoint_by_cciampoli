00f199ac70ba5a575d5daeb880f5bb14b0c40e01
root <root@chris-laptop.(none)>
2014-03-09 23:52:09 -0700

    changes

--- public/js/employees/index.js
+++ public/js/employees/index.js
@@ -2,7 +2,7 @@ $(function() {
     $('#logout_btn').on('click',function(e){
        e.preventDefault();
        $.ajax({
-          url: "<?php echo base_url('auth/logout');?>",
+          url: "auth/logout",
           data: {},
           success: function(data) {
               
