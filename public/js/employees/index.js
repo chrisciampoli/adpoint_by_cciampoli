c61a6ed3ab9d4709035d6879811d595ea5ab240f
root <root@chris-laptop.(none)>
2014-03-10 23:41:56 -0700

    changes

--- public/js/employees/index.js
+++ public/js/employees/index.js
@@ -2,11 +2,30 @@ $(function() {
     
     var logout_url = config.base + "auth/logout";
     
+    var monthNames = ["January", "February", "May", "June", "March", "April", "July", "August", "September", "October", "November", "December"];
+
+    var dayNames = ["S", "M", "T", "W", "T", "F", "S"];
+
     $('#calendar').bic_calendar({
+        //enable select
+        enableSelect: false,
+        //set day names
+        dayNames: dayNames,
+        //set month names
+        monthNames: monthNames,
+        //show dayNames
+        showDays: true,
+        //show month controller
+        displayMonthController: true,
+        //show year controller
+        displayYearController: true,
+        //change calendar to english format
         startWeekDay: 1,
-        enableSelect: true,
-        multiSelect: true,
-        displayYearController: false
+        //set ajax call
+        reqAjax: {
+            type: 'get',
+            url: 'http://bic.cat/bic_calendar/index.php'
+        }
     });
     
     $('#logout_btn').on('click',function(e){
