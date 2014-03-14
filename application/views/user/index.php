7394352bfea30f07a342ecd8f500f41f4b53db05
root <root@chris-laptop.(none)>
2014-03-13 21:36:19 -0700

    changes

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -65,7 +65,7 @@
                     </table>
 
                     <div class="row">
-                        <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">Swift Pick up</button>
+                        <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#pickup_modal">Swift Pick up</button>
                         <button type="button" id="busy_btn" class="btn btn-primary col-xs-6">Busy or Not</button>
                     </div>
                     <div class="row"></div>
@@ -114,19 +114,19 @@
             </div>
         </div>
         <!-- Swift Pick up Modal -->
-        <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal fade" id="pickup_modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
             <div class="modal-dialog">
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+                        <h4 class="modal-title" id="myModalLabel">Swift Pick up</h4>
                     </div>
                     <div class="modal-body">
-                        ...
+                        Would you like to pick up a shift for today?
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+                        <button type="button" class="btn btn-primary">Submit Request</button>
                     </div>
                 </div>
             </div>
