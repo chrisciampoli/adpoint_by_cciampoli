6a1aa3345d5fcab8401147374f13b879c4c7d652
root <root@chris-laptop.(none)>
2014-03-13 20:00:04 -0700

    changes

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -65,7 +65,7 @@
                     </table>
                     
                     <div class="row">
-                        <button type="button" id="pickup_btn" class="btn btn-primary col-xs-6">Swift Pick up</button>
+                        <a href="#myModal" role="button" class="btn" data-toggle="modal">Swift Pick up</a>
                         <button type="button" id="busy_btn" class="btn btn-primary col-xs-6">Busy or Not</button>
                     </div>
                     <div class="row"></div>
@@ -113,8 +113,19 @@
                 </div>
             </div>
         </div>
-        <div id="swift_dialog">
-            
+        <!-- Modal -->
+        <div id="myModal" class="modal hide fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                <h3 id="myModalLabel">Modal header</h3>
+            </div>
+            <div class="modal-body">
+                <p>One fine body…</p>
+            </div>
+            <div class="modal-footer">
+                <button class="btn" data-dismiss="modal" aria-hidden="true">Close</button>
+                <button class="btn btn-primary">Save changes</button>
+            </div>
         </div>
         <!-- Bootstrap core JavaScript
         ================================================== -->
