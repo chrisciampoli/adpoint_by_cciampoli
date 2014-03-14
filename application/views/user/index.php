05629435acc877d7ae86450e484db76356b447b5
root <root@chris-laptop.(none)>
2014-03-13 21:44:17 -0700

    changes

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -67,9 +67,6 @@
                     <div class="row">
                         <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#pickup_modal">Swift Pick up</button>
                         <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#busy_modal">Busy or Not</button>
-                    </div>
-                    <div class="row"></div>
-                    <div class="row">
                         <button class="btn btn-primary btn-lg" id="logout_btn">Logout</button>
                     </div>
                 </div>
