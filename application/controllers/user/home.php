3d8ae862efedec435413c430cba19700f80b0e84
root <root@chris-laptop.(none)>
2014-03-16 16:08:15 -0700

    changes

--- application/controllers/user/home.php
+++ application/controllers/user/home.php
@@ -30,9 +30,15 @@ class Home extends User_Controller {
         }
         $schedule = $this->mdl_schedule->getSchedule($username);
         if(!$schedule) {
-            
+            return json_encode(array(
+               'Status'=>'Failure',
+               'Message'=>'Could not fetch schedule, make sure this employee has one!'
+            ));
         } else {
-            
+            return json_encode(array(
+               'Status'=>'Success',
+               'Message'=>$schedule
+            ));
         }
     }
     
