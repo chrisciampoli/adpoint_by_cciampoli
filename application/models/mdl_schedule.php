3522133621bf26f8e137f67c6e1f75f5a6e7b2ff
root <root@chris-laptop.(none)>
2014-03-16 16:43:22 -0700

    changes

--- application/models/mdl_schedule.php
+++ application/models/mdl_schedule.php
@@ -7,6 +7,8 @@ class Mdl_schedule extends CI_Model {
     }
     
     function getSchedule($username) {
+        $this->db->select('schedule');
+        $this->db->where('user',$username);
         $query = $this->db->get('schedules');
         $result = $query->result_array();
         if($result) {
