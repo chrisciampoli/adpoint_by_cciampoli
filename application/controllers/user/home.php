2921825cd4bd02c1bf51b3896015db2d1b594320
root <root@chris-laptop.(none)>
2014-03-17 11:35:40 -0700

    changes

--- application/controllers/user/home.php
+++ application/controllers/user/home.php
@@ -8,7 +8,8 @@ class Home extends User_Controller {
     }
     
     function index() {
-        $this->load->view('user/index.php');
+        $data['username'] = $this->session->userdata('username');
+        $this->load->view('user/index.php',$data);
     }
     
     function about() {
@@ -34,46 +35,6 @@ class Home extends User_Controller {
         
         return;
         
-    }
-    
-    function postSchedule($username = null, $schedule = null) {
-        $username = $this->session->userdata('username');
-        $schedule = json_decode($this->input->post('schedule'));
-        if($this->mdl_schedule->postSchedule(json_encode($schedule))) {
-            echo json_encode(array('status'=>'success', 'message'=>'schedule saved'));
-            return;
-        } else {
-            echo json_encode(array('status'=>'failure', 'message'=>'schedule could not be saved!'));
-            return;
-        }
-    }
-    
-    function insert() {
-        $username = $this->session->userdata('username');
-        $data = array(
-            'user'=>$username,
-            'schedule'=>array(
-                array(
-                    "date"=>"11/3/2014",
-                    "title"=>"Starbucks: College",
-                    "color"=>"#333",
-                    "content"=>"4:30PM - 10:30PM"
-                ),
-                array(
-                    "date"=>"12/3/2014",
-                    "title"=>"Starbucks: College",
-                    "color"=>"#333",
-                    "content"=>"4:30PM - 10:30PM"
-                ),
-                array(
-                    "date"=>"13/3/2014",
-                    "title"=>"Starbucks: College",
-                    "color"=>"#333",
-                    "content"=>"4:30PM - 10:30PM"
-                )
-            )
-        );
-        $this->db->insert('schedules',json_encode($data));
     }
     
 }
