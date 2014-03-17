21b909dbe15c7e9e6b82053a76d12bb8dba5d4eb
root <root@chris-laptop.(none)>
2014-03-16 17:07:39 -0700

    changes

--- application/controllers/user/home.php
+++ application/controllers/user/home.php
@@ -5,7 +5,6 @@ class Home extends User_Controller {
     function __constructor() {
         parent::_constructor();
         $this->load->library('session');
-        $this->load->model('mdl_schedule');
     }
     
     function index() {
@@ -23,6 +22,7 @@ class Home extends User_Controller {
     function getSchedule($username = null) {
         
         $username = $this->session->userdata('username');
+        $this->load->model('mdl_schedule');
         
         if($username == null) {
             return false;
