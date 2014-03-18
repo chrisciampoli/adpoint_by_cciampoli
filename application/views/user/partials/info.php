4f3d35a6f280e44c069a4d51ff16b9b6ef934a34
root <root@chris-laptop.(none)>
2014-03-17 18:16:51 -0700

    changes

---
+++ application/views/user/partials/info.php
@@ -0,0 +1,14 @@
+<table class="table table-striped table-hover">
+    <tr>
+        <td class="well"><h2><?php echo ucwords($username); ?></h2></td>
+    </tr>
+    <tr>
+        <td class="well"><h3 id="date"></h3></td>
+    </tr>
+    <tr>
+        <td class="well"><h3 id="working">Title - Area</h3></td>
+    </tr>
+    <tr>
+        <td><h3 id="hours">Shift Hours</h3></td>
+    </tr>
+</table>
