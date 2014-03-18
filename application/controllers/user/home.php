96552bbbd1c48f88ecec3cdbb53f726289a84f7b
root <root@chris-laptop.(none)>
2014-03-17 23:06:30 -0700

    changes

--- application/controllers/user/home.php
+++ application/controllers/user/home.php
@@ -11,9 +11,9 @@ class Home extends User_Controller {
         //$this->load->model('ion_auth_model', 'auth');
         $this->db->select('users.username, groups.name');
         $this->db->from('users');
-        $this->db->where('groups.name','members');
         $this->db->join('users_groups','users.id = users_groups.user_id');
         $this->db->join('groups','users_groups.group_id = groups.id');
+        $this->db->where('groups.name','members');
         $query = $this->db->get();
         $data['employees'] = $query->result_array();
         $data['username'] = $this->session->userdata('username');
