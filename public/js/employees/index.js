51bc034988892f373a733b443886d9e3e1c80751
root <root@chris-laptop.(none)>
2014-03-17 17:58:03 -0700

    changes

--- public/js/employees/index.js
+++ public/js/employees/index.js
@@ -1,79 +1,82 @@
 $(function() {
 
     var logout_url = config.base + "auth/logout",
-        schedule_url = config.base + "user/home/getSchedule";
+            schedule_url = config.base + "user/home/getSchedule";
 
     $.ajax({
         url: schedule_url,
         data: {},
         success: function(data) {
             console.log('DATA: ' + data);
-            var parsed = $.parseJSON(data);
-            var events = $.parseJSON(parsed[0].schedule);
-            
-            // We need to do a few things here with the data before
-            // using it to populate the calendar.
-            
-            /*
-             * First we need to get todays date, then check for todays date
-             * within the schedule to see if the user is working today. If not
-             * then we need to change the working and hours sections to "Not Scheduled"
-             * 
-             * If they DO have a schedule for today, then we need to update the working and
-             * hours section to reflect that.
-             */
-            var today = new Date(),
-                month = today.getMonth()+1,
-                myDate = today.getDate() + '/' + month + '/' + today.getFullYear(),
-                working = false,
-                title,
-                hours;
-           
-            $('#date').html(today.toDateString());
-            
-            $.each(events, function(i, e){
-               if(myDate === e.date) {
-                    working = true;
-                    console.log(e);
-                    title = e.title;
-                    hours = e.content;
-               }
-            });
-            
-            
-            if(working === true) {
-                $('#working').html(title);
-                $('#hours').html(hours);
-            } else if(working === false) {
-                $('#working').html('Not scheduled for today');
-                $('#hours').html('*-*');
-            }
-            var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            if (data !== 'false') {
+                var parsed = $.parseJSON(data);
+                var events = $.parseJSON(parsed[0].schedule);
 
-            var dayNames = ["S", "M", "T", "W", "T", "F", "S"];
+                // We need to do a few things here with the data before
+                // using it to populate the calendar.
 
-            $('#calendar').bic_calendar({
-                events: events,
-                //enable select
-                enableSelect: true,
-                //set day names
-                dayNames: dayNames,
-                //set month names
-                monthNames: monthNames,
-                //show dayNames
-                showDays: true,
-                //show month controller
-                displayMonthController: true,
-                //show year controller
-                displayYearController: true,
-                //change calendar to english format
-                startWeekDay: 1,
-                //set ajax call
-                reqAjax: {
-                    type: 'get',
-                    url: 'http://bic.cat/bic_calendar/index.php'
+                /*
+                 * First we need to get todays date, then check for todays date
+                 * within the schedule to see if the user is working today. If not
+                 * then we need to change the working and hours sections to "Not Scheduled"
+                 * 
+                 * If they DO have a schedule for today, then we need to update the working and
+                 * hours section to reflect that.
+                 */
+                var today = new Date(),
+                        month = today.getMonth() + 1,
+                        myDate = today.getDate() + '/' + month + '/' + today.getFullYear(),
+                        working = false,
+                        title,
+                        hours;
+
+                $('#date').html(today.toDateString());
+
+                $.each(events, function(i, e) {
+                    if (myDate === e.date) {
+                        working = true;
+                        console.log(e);
+                        title = e.title;
+                        hours = e.content;
+                    }
+                });
+
+
+                if (working === true) {
+                    $('#working').html(title);
+                    $('#hours').html(hours);
+                } else if (working === false) {
+                    $('#working').html('Not scheduled for today');
+                    $('#hours').html('*-*');
                 }
-            });
+                var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+                var dayNames = ["S", "M", "T", "W", "T", "F", "S"];
+
+                $('#calendar').bic_calendar({
+                    events: events,
+                    //enable select
+                    enableSelect: true,
+                    //set day names
+                    dayNames: dayNames,
+                    //set month names
+                    monthNames: monthNames,
+                    //show dayNames
+                    showDays: true,
+                    //show month controller
+                    displayMonthController: true,
+                    //show year controller
+                    displayYearController: true,
+                    //change calendar to english format
+                    startWeekDay: 1,
+                    //set ajax call
+                    reqAjax: {
+                        type: 'get',
+                        url: 'http://bic.cat/bic_calendar/index.php'
+                    }
+                });
+            }
+
         },
         failure: function(data) {
             alert('Issue with pulling schedule!  Please refresh the page');
