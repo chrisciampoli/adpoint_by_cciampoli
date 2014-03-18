dd549af0af33385581b3faf08bfe574e755bd06d
root <root@chris-laptop.(none)>
2014-03-17 18:35:15 -0700

    changes

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -54,24 +54,7 @@
                 <div class="tab-pane" id="week">
                     <?php $this->load->view('user/week');?>
                 </div>
-                <div class="tab-pane" id="month">
-                    <table class="table table-striped table-hover">
-                        <tr>
-                            <td class="well"><h2><?php echo ucwords($username);?></h2></td>
-                        </tr>
-                        <tr>
-                            <td class="well"><h3 id="date"></h3></td>
-                        </tr>
-                        <tr>
-                            <td class="well"><h3 id="working">Title - Area</h3></td>
-                        </tr>
-                        <tr>
-                            <td><div id="calendar"></div></td>
-                        </tr>
-                    </table>
-                    <div class="row">
-                        <button type="button" class="btn btn-primary col-xs-12" id="logout_btn">Logout</button>
-                    </div>
+                    <?php $this->load->view('user/month');?>
                 </div>
             </div>
         </div>
