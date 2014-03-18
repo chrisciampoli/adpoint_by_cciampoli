447b94aaa1a87ebe5d18515dba91aaf31e3385ff
root <root@chris-laptop.(none)>
2014-03-17 18:29:31 -0700

    changes

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -48,9 +48,66 @@
                 <li><a href="#month" data-toggle="tab">Month</a></li>
             </ul>
             <div class="tab-content">
-                <div class="tab-pane active" id="daily"><?php echo $day;?></div>
-                <div class="tab-pane" id="week"><?php echo $week;?></div>
-                <div class="tab-pane" id="month"><?php echo $month;?></div>
+                <div class="tab-pane active" id="daily">
+                    <table class="table table-striped table-hover">
+                        <tr>
+                            <td class="well"><h2><?php echo ucwords($username);?></h2></td>
+                        </tr>
+                        <tr>
+                            <td class="well"><h3 id="date"></h3></td>
+                        </tr>
+                        <tr>
+                            <td class="well"><h3 id="working">Title - Area</h3></td>
+                        </tr>
+                        <tr>
+                            <td><h3 id="hours">Shift Hours</h3></td>
+                        </tr>
+                    </table>
+
+                    <div class="row">
+                        <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#pickup_modal">Swift Pick up</button>
+                        <button class="btn btn-primary btn-lg" data-toggle="modal" data-target="#busy_modal">Busy or Not</button>
+                        <button class="btn btn-primary btn-lg" id="logout_btn">Logout</button>
+                    </div>
+                </div>
+                <div class="tab-pane" id="week">
+                    <table class="table table-striped table-hover">
+                        <tr>
+                            <td class="well"><h2><?php echo ucwords($username);?></h2></td>
+                        </tr>
+                        <tr>
+                            <td class="well"><h3 id="date"></h3></td>
+                        </tr>
+                        <tr>
+                            <td class="well"><h3 id="working">Title - Area</h3></td>
+                        </tr>
+                        <tr>
+                            <td><h3>Calendar with Week View</h3></td>
+                        </tr>
+                    </table>
+                    <div class="row">
+                        <button type="button" class="btn btn-primary col-xs-12" id="logout_btn">Logout</button>
+                    </div>
+                </div>
+                <div class="tab-pane" id="month">
+                    <table class="table table-striped table-hover">
+                        <tr>
+                            <td class="well"><h2><?php echo ucwords($username);?></h2></td>
+                        </tr>
+                        <tr>
+                            <td class="well"><h3 id="date"></h3></td>
+                        </tr>
+                        <tr>
+                            <td class="well"><h3 id="working">Title - Area</h3></td>
+                        </tr>
+                        <tr>
+                            <td><div id="calendar"></div></td>
+                        </tr>
+                    </table>
+                    <div class="row">
+                        <button type="button" class="btn btn-primary col-xs-12" id="logout_btn">Logout</button>
+                    </div>
+                </div>
             </div>
         </div>
         <!-- Swift Pick up Modal -->
