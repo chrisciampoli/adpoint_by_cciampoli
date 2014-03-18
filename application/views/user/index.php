50cab157c0c545ef8ed2afc41951fb56fece12a9
root <root@chris-laptop.(none)>
2014-03-17 19:16:29 -0700

    changes

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -65,10 +65,10 @@
                 <div class="modal-content">
                     <div class="modal-header">
                         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                        <h4 class="modal-title" id="myModalLabel">Swift Pick up</h4>
+                        <h4 class="modal-title" id="myModalLabel">Swift Give up</h4>
                     </div>
                     <div class="modal-body">
-                        Would you like to pick up a shift for today?
+                        Would you like to give up your shift for today?
                     </div>
                     <div class="modal-footer">
                         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
