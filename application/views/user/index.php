0e732e18b37cc596583a935781da4a90915bcdd8
chrisciampoli <chrisciampoli@gmail.com>
2014-03-18 13:43:39 -0700

    changes

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -69,10 +69,10 @@
                         </thead>
                         <tbody>
                             <tr class="warning pending-row" rel="1">
-                                <td>Edward</td><td>3/18/14</td><td>Mission Valley</td><td>4:30PM - 10:30PM</td><td><span class="label label-warning">Pending</span></td>
+                                <td>Edward</td><td>3/18/14</td><td>Mission Valley</td><td>4:30PM - 10:30PM</td><td><span class="label label-success">Accepted</span></td>
                             </tr>
                             <tr class="success accepted-row" rel="2">
-                                <td>Jeff</td><td>3/19/14</td><td>Mission Valley</td><td>4:30PM - 10:30PM</td><td><span class="label label-success">Accepted</span></td>
+                                <td>Jeff</td><td>3/19/14</td><td>Mission Valley</td><td>4:30PM - 10:30PM</td><td><span class="label label-info">Accepted and Scheduled</span></td>
                             </tr>
                             <tr class="danger denied-row" rel="3">
                                 <td>Ian</td><td>3/20/14</td><td>Mission Valley</td><td>4:30PM - 10:30PM</td><td><span class="label label-danger">Denied</span></td>
