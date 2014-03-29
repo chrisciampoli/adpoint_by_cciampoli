1aa9f157fa7d5eacad66c85b1b3f21973669f5e7
Chris <chris@chris-laptop.(none)>
2014-03-28 21:07:06 -0700

    changes

--- application/views/public/index.php
+++ application/views/public/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
     <head>
         <meta charset="utf-8">
-        <title>Swift Schedules, by Quick and Co</title>
+        <title>Swift Shifts, by SwiftNCo</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="">
         <meta name="author" content="">
@@ -38,7 +38,7 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#">Swift Schedules</a>
+                    <a class="navbar-brand" href="#">Swift shifts</a>
                 </div>
                 <div class="navbar-collapse collapse">
                     <?php $attributes = array('class' => 'navbar-form navbar-right', 'id' => 'signin_form'); ?>
@@ -61,7 +61,7 @@
         <div class="jumbotron">
             <div class="container">
                 <h1>Manage your time better!</h1>
-                <p>Swift Schedules is a software platform that aims to become the industry standard for providing a simple and intuitive application for managing schedules.</p>
+                <p>Swift shifts is a software platform that aims to become the industry standard for providing a simple and intuitive application for managing shifts.</p>
                 <p><a class="btn btn-primary btn-lg" role="button">Learn more &raquo;</a></p>
             </div>
         </div>
@@ -72,7 +72,7 @@
                 <div class="col-md-4">
                     <h2>Benefits the Employee</h2>
                     <ul>
-                        <li>Providing 24/7 online access to their schedules via computer or smartphone</li>
+                        <li>Providing 24/7 online access to their shifts via computer or smartphone</li>
                         <li>Empowering employees to pick up a shift if they do not work that day (Swift Pick-Up)</li>
                         <li>Introducing 'Busy or Not'.  Employees press 'Busy or Not' and if not, do not have to work that shift.</li>
                     </ul>
@@ -81,7 +81,7 @@
                 <div class="col-md-4">
                     <h2>Saves Employers Money</h2>
                     <ul>
-                        <li>Saving managers' time and pay from creating, adjusting, managing all employee schedules</li>
+                        <li>Saving managers' time and pay from creating, adjusting, managing all employee shifts</li>
                         <li>Keeping managers on the sales floor with customers instead of on the phone or computer for scheduling</li>
                         <li>Allowing same chain storefronts to communicate and analyze daily, weekly, and monthly labor figures</li>
                     </ul>
