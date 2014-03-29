0b434ccc43728007d6b9986705b1206a6703acfe
Chris <chris@chris-laptop.(none)>
2014-03-28 21:04:23 -0700

    changes

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://swiftnco.com/swift_schedules/';
+$config['base_url']	= 'http://swiftnco.com/swiftshifts/';
 
 /*
 |--------------------------------------------------------------------------
