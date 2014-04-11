a187771d4f480cbf2a1e069afd414fcc56bb41db
Christopher Ciampoli <chrisciampoli@github.com>
2014-04-10 18:33:06 -0700

    changes

--- application/views/manager/pages/employees2.php
+++ application/views/manager/pages/employees2.php
@@ -84,23 +84,39 @@
       <div class="modal-body">
         <form class="form-horizontal">
           <div class="control-group">
+            <label class="control-label" for="inputFirstName">First Name</label>
+            <div class="controls">
+              <input type="text" id="inputFirstName" placeholder="John">
+            </div>
+          </div>
+          <div class="control-group">
+            <label class="control-label" for="inputLastName">Last Name</label>
+            <div class="controls">
+              <input type="text" id="inputLastName" placeholder="Doe">
+            </div>
+          </div>
+          <div class="control-group">
             <label class="control-label" for="inputEmail">Email</label>
             <div class="controls">
-              <input type="text" id="inputEmail" placeholder="Email">
+              <input type="email" id="inputEmail" placeholder="user@example.com">
+            </div>
+          </div>
+          <div class="control-group">
+            <label class="control-label" for="inputPhone">Phone</label>
+            <div class="controls">
+              <input type="text" id="inputPhone" placeholder="(000) 000-0000">
             </div>
           </div>
           <div class="control-group">
             <label class="control-label" for="inputPassword">Password</label>
             <div class="controls">
-              <input type="password" id="inputPassword" placeholder="Password">
+              <input type="password" id="inputPassword" placeholder="">
             </div>
           </div>
           <div class="control-group">
+            <label class="control-label" for="inputPasswordConfirmation">Re-enter Password</label>
             <div class="controls">
-              <label class="checkbox">
-                <input type="checkbox"> Remember me
-              </label>
-              <button type="submit" class="btn">Sign in</button>
+              <input type="password" id="inputPasswordConfirmation" placeholder="">
             </div>
           </div>
         </form>
