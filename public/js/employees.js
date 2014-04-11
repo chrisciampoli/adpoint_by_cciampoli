548496efcc5b7bd2a5173abcb656ae4192fe23da
Christopher Ciampoli <chrisciampoli@github.com>
2014-04-10 18:55:18 -0700

    changes

--- public/js/employees.js
+++ public/js/employees.js
@@ -1,41 +1,5 @@
 $(function(){ 
-//    getEmployees(); 
-   /*
-    $(".check_avail").on('click', function(e) {
-        e.preventDefault();
-        /*
-       
-        $("#datepicker_" + rel).datepicker({
-            showButtonPanel: true,
-            buttonText: "Done",
-            onSelect: function() {
-                $('<div>Location</div>').dialog();
-            }
-        });
-        */
-        /*
-        var rel = $(this).attr('rel');
-        $('#avail_'+rel).fadeToggle();
-    });
-    */
-    var add_employee = String()
-            + '<section>'
-                + '<div>'
-                        + '<div><label for="first_name">First Name</label><input type="text" name="first_name" id="first_name" /></div>'
-                        + '<div><label for="last_name">Last Name</label><input type="text" name="last_name" id="last_name" /></div>'
-                        + '<div><label for="position">Position</label><input type="text" name="position" id="position" /></div>'
-                        + '<div><label for="phone">Phone</label><input type="text" name="phone" id="phone" /></div>'
-                        + '<div><label for="email">Last Name</label><input type="text" name="email" id="email" /></div>'
-                        + '<div><label for="availability">Availability</label><input type="text" name="availability" id="availability" /></div>'
-                + '</div>'
-            + '</section>';
-       
-    $('body').on('click', 'input#add_employee_btn', function(e) {
-        $(add_employee).dialog({
-            title: "Add Employee"
-        });
-    });
-
+    
     Date.prototype.getWeek = function(start)
     {
             //Calcing the starting point
