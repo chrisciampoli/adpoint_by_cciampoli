35bbb4bc0b1c463b9d95422d9ae18148d33a3f22
chrisciampoli <chrisciampoli@gmail.com>
2014-04-14 21:24:56 -0700

    changes

--- application/views/manager/pages/employees2.php
+++ application/views/manager/pages/employees2.php
@@ -113,7 +113,7 @@
       </div>
       <div class="modal-footer">
         <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-primary" id="scheduleSaveBtn">Save changes</button>
       </div>
     </div>
   </div>
