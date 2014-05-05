6397d0f545a9b7f4fbaa007e7bd90f4654af033a
chrisciampoli <chrisciampoli@gmail.com>
2014-05-04 20:19:24 -0700

    changes

--- application/views/manager/pages/settings.php
+++ application/views/manager/pages/settings.php
@@ -13,22 +13,9 @@
             <option value="fashion valley">Fashion Valley</option>
             <option value="SDSU">SDSU</option>
         </select>
-        <button type="button" class="btn btn-default" id="add_location">Add Location</button>
+        <button type="button" class="btn btn-default" id="add_location">Edit Location</button>
+        <button type="button" class="btn btn-default" id="add_location">Add New Location</button>
       </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-      </div>
-      <div class="form-group">
-        <label for="exampleInputFile">File input</label>
-        <input type="file" id="exampleInputFile">
-        <p class="help-block">Example block-level help text here.</p>
-      </div>
-      <div class="checkbox">
-        <label>
-          <input type="checkbox"> Check me out
-        </label>
-      </div>
-      <button type="submit" class="btn btn-default">Submit</button>
+      <button type="submit" class="btn btn-default">Save</button>
     </form>
 </div>
