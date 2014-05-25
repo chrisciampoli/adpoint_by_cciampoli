fa4c75ad9072ce2d86015a5574a4731e46096d43
chrisciampoli <chrisciampoli@gmail.com>
2014-05-24 18:41:12 -0700

    changes

--- application/views/manager/pages/settings.php
+++ application/views/manager/pages/settings.php
@@ -1,11 +1,9 @@
 <script>
     var config = {
         base: "<?php echo base_url(); ?>",
-        notes: 4
     };
 </script>
 <div class="container-fluid">
-    <form role="form" method="post" action="<?= base_url('manager/home/postSettings');?>">
       <div class="form-group">
         <label for="company_name">Company Name</label>
         <input value="<?=(isset($settings[0]['company_name']) ? $settings[0]['company_name'] : '');?>" type="input" class="form-control" id="company_name" name="company_name" placeholder="Company Name" required>
@@ -55,6 +53,5 @@
         <label for="admin_email">Administrator Contact Email</label>
         <input value="<?=(isset($settings[0]['admin_email']) ? $settings[0]['admin_email'] : '');?>" name="admin_email" type="email" class="form-control" id="admin_email" placeholder="admin@example.com" required>
       </div>
-      <button type="submit" class="btn btn-default">Save</button>
-    </form>
+      <button class="btn btn-default" id="saveBtn">Save</button>
 </div>
