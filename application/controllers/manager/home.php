85d4fb9530aab7358072e148671ff4ec7784b5ae
chrisciampoli <chrisciampoli@gmail.com>
2014-08-12 22:24:40 -0700

    changes

--- application/controllers/manager/home.php
+++ application/controllers/manager/home.php
@@ -103,7 +103,7 @@ class Home extends Manager_Controller {
         $data['request_count'] = $this->request_count;
         $data['settings'] = $this->getSettings($this->company);
         //$data['shifts'] = (isset($data['settings'][0]['shifts']) ? $data['settings'][0]['shifts'] : '{"result":"empty"}');
-        /*
+        
         if(!empty($data['settings'])) {
 
             if($data['settings'][0]['shifts'] != "false") {
@@ -123,7 +123,7 @@ class Home extends Manager_Controller {
         } else {
             $data['locations'] = $data['settings'][0]['locations'];
         }
-        */
+        
 
         $data['display_name'] = $this->display_name;
         $data['head'] = $this->load->view('manager/head', $data, true);
