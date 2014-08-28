315e71bcb42e994da18398be4095d0022308e9d4
unknown <cciampoli@cciampoli-dev.hq.isafe.org>
2014-08-28 10:39:55 -0700

    changes

--- application/views/manager/head.php
+++ application/views/manager/head.php
@@ -9,18 +9,5 @@
     <link rel="shortcut icon" href="../../assets/ico/favicon.ico">
 
     <title><?=$title;?></title>
-    <?php $this->load->view('manager/styles', $styles);?>
-   
-    <!-- Just for debugging purposes. Don't actually copy this line! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-
-    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <script src="<?=base_url('public/js/swift.js');?>"></script>
-    <script src='https://code.jquery.com/jquery-1.10.2.min.js'></script>
-    <script src="<?=base_url('public/js/libs/bootstrap.min.js');?>"></script>
-    <script src="http://code.jquery.com/ui/1.10.4/jquery-ui.js"></script>;
+    
   </head>
